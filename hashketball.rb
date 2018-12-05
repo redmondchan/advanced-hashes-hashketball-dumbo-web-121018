57d36c503e986c028003997a9e7df8608c3f4b33
Redmond Chan <redmondchan92@gmail.com>
2018-12-05 05:24:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,18 @@
 # Write your code here!
-
+def game_hash = {
+  :home = {
+    :team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => {
+      :alan_anderson => {number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"},
+      :reggie_evans => {number: "30", shoe: "14", points: "12", rebounds: "12", assists: "12", steals: "12", blocks: "12", slam_dunks: "7"},
+      :brook_lopez => {number: "11", shoe: "17", points: "17", rebounds: "19", assists: "10", steals: "3", blocks: "1", slam_dunks: "15"},
+      :mason_plumlee => {number: "1", shoe: "19", points: "26", rebounds: "12", assists: "6", steals: "3", blocks: "8", slam_dunks: "5"},
+      :jason_terry  => {number: "31", shoe: "15", points: "19", rebounds: "2", assists: "2", steals: "4", blocks: "11", slam_dunks: "1"}
+    }
+  }
+  :away = {}
+}
 
 
 
