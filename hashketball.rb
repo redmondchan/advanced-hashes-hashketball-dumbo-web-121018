5f4a4f1230828195accd72658e5e6995b946a2b7
Redmond Chan <redmondchan92@gmail.com>
2018-12-05 05:56:00 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,28 @@
 # Write your code here!
-def game_hash = {
-  :home = {
-    :team_name => "Brooklyn Nets",
+require pry
+
+def good_practices
+  game_hash.each do |location, team_data|
+    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+    binding.pry
+      team_data.each do |attribute, data|
+        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+        binding.pry
+
+        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+        data.each do |data_item|
+            binding.pry
+      end
+    end
+  end
+end
+
+good_practices
+
+def game_hash
+  nba = {
+  :home => {
+    :team_name => ("Brooklyn Nets"),
     :colors => ["Black", "White"],
     :players => {
       :alan_anderson => {number: "0", shoe: "16", points: "22", rebounds: "12", assists: "12", steals: "3", blocks: "1", slam_dunks: "1"},
@@ -10,15 +31,24 @@ def game_hash = {
       :mason_plumlee => {number: "1", shoe: "19", points: "26", rebounds: "12", assists: "6", steals: "3", blocks: "8", slam_dunks: "5"},
       :jason_terry  => {number: "31", shoe: "15", points: "19", rebounds: "2", assists: "2", steals: "4", blocks: "11", slam_dunks: "1"}
     }
+  },
+  :away => {
+    :team_name => ("Charlotte Hornets"),
+    :colors => ["Turquoise", "Purple"],
+    :players => {
+      :jeff_adrien => {number: "4", shoe: "18", points: "10", rebounds: "1", assists: "1", steals: "2", blocks: "7", slam_dunks: "2"},
+      :bismak_biyombo => {number: "0", shoe: "16", points: "12", rebounds: "4", assists: "7", steals: "7", blocks: "15", slam_dunks: "10"},
+      :desagna_diop => {number: "2", shoe: "14", points: "24", rebounds: "12", assists: "12", steals: "4", blocks: "5", slam_dunks: "5"},
+      :ben_gordon => {number: "8", shoe: "15", points: "33", rebounds: "3", assists: "2", steals: "1", blocks: "1", slam_dunks: "0"},
+      :brendan_haywood  => {number: "33", shoe: "15", points: "6", rebounds: "12", assists: "12", steals: "22", blocks: "5", slam_dunks: "12"}
   }
-  :away = {}
 }
+}
+end
 
-
-
-
-
-
-
-
-
+def num_points_scored(name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      data.each do |data_item|
+        name
+end  
